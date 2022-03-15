ca5cd5d4ad136e94ca0235482854cd195fa49148
sathish <sathish1048@gmail.com>
2022-03-16 00:05:20 +0530

    deploy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 
 export interface Users{
   userName : string;
