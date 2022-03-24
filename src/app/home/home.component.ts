8cb8be875a1ab523747014ab5ff6e8676ca92658
Prashanth-694 <prashanthmp009@gmail.com>
2022-03-24 12:37:46 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,6 +24,7 @@ export class HomeComponent implements AfterViewInit {
   userId : any;
   name : any;
   role : any;
+  isPie : boolean = true;
   isDashbord : boolean = true;
   myChart : any;
   constructor(
@@ -81,7 +82,7 @@ export class HomeComponent implements AfterViewInit {
           const canvas = <HTMLCanvasElement> document.getElementById('myChart');
           const ctx : any = canvas.getContext('2d');
           this.myChart = new Chart(ctx, {
-            type: 'pie',
+            type: 'bar',
             data: {
                 labels: users.map((item:any)=>{ return item.userName}),
                 datasets: [{
@@ -114,6 +115,42 @@ export class HomeComponent implements AfterViewInit {
                 }
             }
             })
+            const canvas1 = <HTMLCanvasElement> document.getElementById('myChart1');
+            const ctx1 : any = canvas1.getContext('2d');
+            this.myChart = new Chart(ctx1, {
+              type: 'pie',
+              data: {
+                  labels: users.map((item:any)=>{ return item.userName}),
+                  datasets: [{
+                      label: 'Investment',
+                      data: users.map((item:any)=>{ return item.spentAmount}),
+                      backgroundColor: [
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(105, 180, 64, 0.2)'
+                      ],
+                      borderColor: [
+                          'rgba(255, 99, 132, 1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)'
+                      ],
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  scales: {
+                      // y: {
+                      //     beginAtZero: false
+                      // }
+                  }
+              }
+              })
           this.myChart.data.labels = users.map((item:any)=>{ return item.userName});
           this.myChart.data.datasets[0].data = users.map((item:any)=>{ return item.spentAmount});
         })
