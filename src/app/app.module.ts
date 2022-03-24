a515b0fecfb3e8cf9a546e40ca0b6321929ae549
Prashanth-694 <prashanthmp009@gmail.com>
2022-03-24 19:11:46 +0530

    cahnges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -23,11 +28,16 @@ import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
     ForgotPwdComponent
   ],
   imports: [
+    MatInputModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    NgChartsModule
+    NgChartsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
